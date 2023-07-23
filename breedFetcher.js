cad7afb440371fb925af7196dbb9cd46060b20e5
Rose Francis <rosennabs@gmail.com>
2023-07-23 15:25:57 -0600

    Refactor cat as a service and test with mocha

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,32 +1,30 @@
 const request = require("request");
 
 //Gets the description of the cat breed.
-const fetchBreedDescription = function(breedName, callback) {
+const fetchBreedDescription = function (breedName, callback) {
   const searchUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
   request(searchUrl, (error, response, body) => {
+    
     if (error) {
       callback("Request Failed", null);
-      process.exit;
-    } else {
-      const data = JSON.parse(body); //converts JSON string to an object
+      process.exit();
+    } 
+    const data = JSON.parse(body); //converts JSON string to an object
+    
 
-      const breedExists = function() {
-        //iterates through the resource array and compares the breed id or name to cmdline argument
-        for (let obj of data) {
-          if (
-            obj.name.toLowerCase() === breedName.toLowerCase() ||
-            obj.id.toLowerCase() === breedName.toLowerCase()
-          ) {
-            return true;
-          }
-        }
-      };
+    if (data.length === 0) {
+      return callback("Breed not found in the resource.", null);
+    }
 
-      if (breedExists()) {
+    
+    //iterates through the resource array and compares the breed id or name to cmdline argument
+    for (let obj of data) {
+      if (
+        obj.name.toLowerCase() === breedName.toLowerCase() ||
+        obj.id.toLowerCase() === breedName.toLowerCase()
+      ) {
         callback(null, obj.description);
-      } else {
-        callback("Breed not found in the resource.", null);
       }
     }
   });
